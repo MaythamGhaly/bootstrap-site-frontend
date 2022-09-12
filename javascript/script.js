6d5026f30feb343e2c0793e16d8dc7cc586c03f2
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-13 00:10:41 +0300

    add comment to the code

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,9 @@
 window.onload = () => {
     const btn=document.getElementById("btn")
-    
-    const email =document.getElementById("email")
-    const phone=document.getElementById("phone")
-    const msg=document.getElementById("msg")
 
     btn.addEventListener("click",check)
     
-
+// start the check fumction
     function check(){
         const name=document.getElementById("full-name")
         const fail=document.getElementById("fail")
@@ -15,16 +11,19 @@ window.onload = () => {
         const email=document.getElementById("email")
         const phone=document.getElementById("phone")
         const msg=document.getElementById("msg")
+        // check for the name
         if(name.value.length < 5) {
             name.style.backgroundColor="red"
             fail.style.display="block"
             fail.innerText="Full Names should be a minimum of 5 character"
         }
+        // check for the email
         else if (at<=2 || (email.value.length-at<=5)){
             email.style.backgroundColor="red"
             fail.style.display="block"
             fail.innerText="Email field should be an email, with a minimum of 3 characters before the @ and 5 characters after the @"
         }
+        // check for the phone number
         else if (phone.value.substring(0,4)!="+961"){
             phone.style.backgroundColor="red"
             fail.style.display="block"
@@ -42,6 +41,7 @@ window.onload = () => {
                 fail.innerText="please enter the right number: +9613xxxxxx or +96171/76/70/xxxxxx"
             }
         }
+        // check for the massege 
         else if (msg.value.length<100){
             console.log("yes")
             msg.style.backgroundColor="red"
