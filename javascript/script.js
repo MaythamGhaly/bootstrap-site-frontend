0f81751b17aa3453e44f586a29058b7671d20cd7
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-14 11:01:40 +0300

    save-data-to-db

--- javascript/script.js
+++ javascript/script.js
@@ -23,24 +23,25 @@ window.onload = () => {
             fail.style.display="block"
             fail.innerText="Email field should be an email, with a minimum of 3 characters before the @ and 5 characters after the @"
         }
+        // I've stoped this because when i enter the phone number "96171505680" the data base give me this warning ("Please enter a value less than or equal to 2147483647")
         // check for the phone number
-        else if (phone.value.substring(0,4)!="+961"){
-            phone.style.backgroundColor="red"
-            fail.style.display="block"
-            fail.innerText="Phone Number should start with +961"
-        }
-        else if (phone.value.substring(0,4)=="+961"){
-            if(phone.value[4]==3 && phone.value.length!=11){
-                phone.style.backgroundColor="red"
-                fail.style.display="block"
-                fail.innerText="please enter the right number: +9613xxxxxx or +96171/76/70/xxxxxx"
-            }
-            else if (phone.value[4]!=3 && phone.value.length!=12){
-                phone.style.backgroundColor="red"
-                fail.style.display="block"
-                fail.innerText="please enter the right number: +9613xxxxxx or +96171/76/70/xxxxxx"
-            }
-        }
+        // else if (phone.value.substring(0,4)!="+961"){
+        //     phone.style.backgroundColor="red"
+        //     fail.style.display="block"
+        //     fail.innerText="Phone Number should start with +961"
+        // }
+        // else if (phone.value.substring(0,4)=="+961"){
+        //     if(phone.value[4]==3 && phone.value.length!=11){
+        //         phone.style.backgroundColor="red"
+        //         fail.style.display="block"
+        //         fail.innerText="please enter the right number: +9613xxxxxx or +96171/76/70/xxxxxx"
+        //     }
+        //     else if (phone.value[4]!=3 && phone.value.length!=12){
+        //         phone.style.backgroundColor="red"
+        //         fail.style.display="block"
+        //         fail.innerText="please enter the right number: +9613xxxxxx or +96171/76/70/xxxxxx"
+        //     }
+        // }
         // check for the massege 
         else if (msg.value.length<100){
             console.log("yes")
@@ -48,5 +49,19 @@ window.onload = () => {
             fail.style.display="block"
             fail.innerText="Message should be a minimum of 100 characters."
         }
+        else {
+            fail.style.color="green"
+            fail.style.display="block"
+            fail.innerText="Message sent!."
+            fetch("link", {method:'post',body: new URLSearchParams({
+                name:name.value,
+                email:email.value,
+                phone:phone.value,
+                message:message.value,
+            })})
+                .then(Response=>Response.json())
+                .then(data=>)
+        }
     }
+
 }
