a46a162d64cccf9db5397f5635e961a690f037aa
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-14 12:22:30 +0300

    add-showfitch-and-create=new-page

---
+++ javascript/scriptmessages.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+
+    fetch(`http://localhost/messages/showmessages.php`)
+    .then(res=>res.json())
+    .then(data=> {
+      for (let i=0; i< data.length ; i++){
+        console.log(data[i].message)
+        let div= document.createElement('p')
+        div.classList.add("msg-style")
+        div.innerHTML=data[i].message
+        document.body.appendChild(div)
+      }
+      })
+
+}
